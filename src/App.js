e0ab39b3ea850a9cc78db8420f2c39bd0b07c0e5
Luiz Pessatto <100043153+luizpessatto@users.noreply.github.com>
2022-07-29 16:33:39 -0300

    searchcep
    React projeto para praticar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import {FiSearch} from 'react-icons/fi';
+import './styles.css';
+import api from './services/api';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [input, setinput] = useState('');
+    const [cep, setCep] = useState({});
+
+    async function handleSearch() {
+        if (input == '') {
+            alert("CEP Inválido!")
+            return;
+        }
+        try {
+            const response = await api.get(`${input}/json`);
+            setCep(response.data)
+            setinput('');
+        } catch  {
+            alert("Ops, CEP não encontrado!")
+            setinput("")
+        }
+
+    }
+
+    return (
+        <div className="container">
+            <h1 className="title">Buscador CEP</h1>
+
+            <div className="containerInput">
+                <input
+                    type="text"
+                    placeholder="Digite o CEP aqui..."
+                    value={input}
+                    onChange={(e) => setinput(e.target.value)}/>
+                <button className="buttonSearch" onClick={handleSearch}>
+                    <FiSearch size={25} color="#FFF"/>
+                </button>
+            </div>
+
+            {
+                Object
+                    .keys(cep)
+                    .length > 0 && (
+                        <main className="main">
+                            <h2>CEP: {cep.cep}</h2>
+                            <span>{cep.logradouro}</span>
+                            <span>
+                                Complemento: {cep.complemento}</span>
+                            <span>{cep.bairro}</span>
+                            <span>{cep.localidade}
+                                - {cep.uf}</span>
+                            <span>DDD:{cep.ddd}</span>
+                        </main>
+                    )
+            }
+
+        </div>
+    );
 }
 
 export default App;
